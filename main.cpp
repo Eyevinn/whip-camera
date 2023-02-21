82c63b174be3c585363249b1fd6671b9475d1c93
oshinongit <oliver.shin@eyevinn.se>
2023-02-21 14:26:49 +0100

    feat: signal to destruct on ctrl+c

--- main.cpp
+++ main.cpp
@@ -37,18 +37,21 @@ struct Connection
 };
 
 GMainLoop* mainLoop = nullptr;
+void intSignalHandler(int32_t)
+{
+    g_main_loop_quit(mainLoop);
+}
 
 int32_t main(int32_t argc, char** argv)
 {
     printf("init\n");
     setenv("GST_PLUGIN_PATH", "/usr/local/lib/gstreamer-1.0", 0);
-    setenv("GST_V4L2_USE_LIBV4L2", "1", 0);
     gst_init(nullptr, nullptr);
-    mainLoop = g_main_loop_new(nullptr, FALSE);
+    void padAdded(GstElement * src, GstPad * newPad, Connection * connection);
 
     Connection connection;
 
-    //Create elements
+    // Create elements
     connection.pipeline_ = gst_pipeline_new("pipeline");
 
     connection.sourceElement_ = gst_element_factory_make("autovideosrc", "source");
@@ -58,16 +61,63 @@ int32_t main(int32_t argc, char** argv)
         return 1;
     }
 
-    connection.sinkElement_ = gst_element_factory_make("autovideosink", "sink");
+    connection.sinkElement_ = gst_element_factory_make("webrtcbin", "sink");
     if (!connection.sinkElement_)
     {
         printf("Failed to make element sink\n");
         return 1;
     }
 
-    //Release gst
+    // Add elements
+    if (!gst_bin_add(GST_BIN(connection.pipeline_), connection.sourceElement_))
+    {
+        printf("Failed to add element source. Note: GST_PLUGIN_PATH needs to be set as described in the README\n");
+        return 1;
+    }
+
+    if (!gst_bin_add(GST_BIN(connection.pipeline_), connection.sinkElement_))
+    {
+        printf("Failed to add element gli_sink\n");
+        return 1;
+    }
+
+    // Signals
+    g_signal_connect(connection.sourceElement_, "pad-added", G_CALLBACK(padAdded), &connection);
+
+    {
+        struct sigaction sigactionData = {};
+        sigactionData.sa_handler = intSignalHandler;
+        sigactionData.sa_flags = 0;
+        sigemptyset(&sigactionData.sa_mask);
+        sigaction(SIGINT, &sigactionData, nullptr);
+    }
+
+    // Start playing
+    printf("Start playing...\n");
+    if (gst_element_set_state(connection.pipeline_, GST_STATE_PLAYING) == GST_STATE_CHANGE_FAILURE)
+    {
+        printf("Unable to set the pipeline to the playing state\n");
+        return 1;
+    }
+
+    printf("Looping...\n");
+    mainLoop = g_main_loop_new(nullptr, FALSE);
+    g_main_loop_run(mainLoop);
+
+    // Release gst
     g_main_loop_unref(mainLoop);
+    gst_element_set_state(connection.pipeline_, GST_STATE_NULL);
     gst_deinit();
     printf("deinit\n");
     return 0;
+}
+
+void padAdded(GstElement* src, GstPad* newPad, Connection* connection)
+{
+    printf("Received new pad '%s' from '%s'\n", GST_PAD_NAME(newPad), GST_ELEMENT_NAME(src));
+
+    if (!gst_element_link_many(connection->sourceElement_, connection->sinkElement_, nullptr))
+    {
+        printf("Failed to link source to sink\n");
+    }
 }
