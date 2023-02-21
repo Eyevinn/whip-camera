70320c7fa86d1611d82f2aa837763599052fe19c
oshinongit <oliver.shin@eyevinn.se>
2023-02-21 12:57:54 +0100

    fix: init main loop

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #define GST_USE_UNSTABLE_API 1// Removes compile warning
 
+//#include "Connection.h"
 #include "nlohmann/json.hpp"
 #include <csignal>
 #include <cstdint>
@@ -17,11 +18,12 @@ GMainLoop* mainLoop = nullptr;
 
 int32_t main(int32_t argc, char** argv)
 {
+    printf("Hello world\n");
+    setenv("GST_PLUGIN_PATH", "/usr/local/lib/gstreamer-1.0", 0);
     gst_init(nullptr, nullptr);
+    mainLoop = g_main_loop_new(nullptr, FALSE);
     g_main_loop_unref(mainLoop);
     gst_deinit();
-
-    printf("Hello world\n");
-
+    printf("deinit\n");
     return 0;
 }
